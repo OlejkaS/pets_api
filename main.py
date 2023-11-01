7668e8b9734acd4525e81f67e156585354952fb1
Олег Силецкий <siletskiyoleg@gmail.com>
2023-11-01 20:05:01 +0300

    refactoring code

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from collections import OrderedDict
 from datetime import datetime
 from fastapi import FastAPI
 from schemas import DeletePetModel, PetBaseModel, PetListWithCount
@@ -8,39 +9,36 @@ app = FastAPI(
 )
 
 
-fake_pets = []
+fake_db = []
+id = 1
 
 
 @app.post('/pets')
 def add_pet(pets: PetBaseModel):
-    pets = dict(pets)
+    global id
+    pets = OrderedDict(pets)
     pets['created_at'] = datetime.now()
-    fake_pets.append(pets)
+    pets['id'] = id
+    fake_db.append(pets)
+    pets.move_to_end('id', last=False)
+    id += 1
     return pets
 
 
 @app.get('/pets', response_model=PetListWithCount)
 def list_pets(limit: int = 20):
-    return {'count': len(fake_pets), 'items': fake_pets[:limit]}
+    return {'count': len(fake_db), 'items': fake_db}
 
 
 @app.delete('/pets')
 def delete_pet(ids: DeletePetModel):
     delete_count = 0
     errors = []
-    found = False
     for id in dict(ids)['ids']:
-        if not fake_pets:
-            errors.append(
-                    {
-                        'id': id,
-                        'error': 'Pet with the matching ID was not found.'
-                    }
-                )
-            continue
-        for num, pet in enumerate(fake_pets):
+        found = False
+        for num, pet in enumerate(fake_db):
             if id == pet['id']:
-                fake_pets.pop(num)
+                fake_db.pop(num)
                 delete_count += 1
                 found = True
                 break
@@ -51,5 +49,4 @@ def delete_pet(ids: DeletePetModel):
                     'error': 'Pet with the matching ID was not found.'
                 }
             )
-            break
     return {'deleted': delete_count, 'errors': errors}
