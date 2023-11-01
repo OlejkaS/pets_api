e12e07125c7a8f1d5d1c9803d9422f411a6eb4a1
Олег Силецкий <siletskiyoleg@gmail.com>
2023-11-01 23:27:10 +0300

    refactor models

--- models.py
+++ models.py
@@ -3,9 +3,11 @@ from datetime import datetime
 from sqlalchemy import (
     Column,
     DateTime,
+    Enum,
     Integer,
     String
 )
+from schemas import PetType
 
 
 class Pet(Base):
@@ -14,5 +16,5 @@ class Pet(Base):
     id: int = Column(Integer, primary_key=True, index=True)
     name: str = Column(String, nullable=False)
     age: int = Column(Integer, nullable=False)
-    type: str = Column(String, nullable=False)
+    type: PetType = Column(Enum(PetType), nullable=False)
     created_at: datetime = Column(DateTime, default=datetime.now)
