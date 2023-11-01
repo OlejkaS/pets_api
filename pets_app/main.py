4206ca3976fae0bc7226a70cbf14b92139a169ce
Олег Силецкий <siletskiyoleg@gmail.com>
2023-11-02 00:46:15 +0300

    sort import

--- pets_app/main.py
+++ pets_app/main.py
@@ -1,9 +1,9 @@
-from db.database import engine, SessionLocal
 from fastapi import Depends, FastAPI
 from sqlalchemy.orm import Session
 
 import api.models as models
 from api.schemas import DeletePetModel, PetBaseModel, PetListWithCount
+from db.database import engine, SessionLocal
 
 
 app = FastAPI(
